03d91a226eaad0d851ba9d3cc6df53e282b0c444
Artur Sobieraj <artur.sobieraj126@gmail.com>
2022-01-23 15:09:24 +0100

    Managing rockets

--- SomeProgram.java
+++ SomeProgram.java
@@ -5,4 +5,8 @@ public class SomeProgram
       System.out.println("We are learning to use Git.");
       System.out.println("We will be Git experts, soon.");
     }
+	
+	public static void manage-rockets() {
+		//TODO
+	}
 }
